6fa9ae27417a23f352d9ed563464dacfa54e4d70
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-08-02 15:56:51 +0530

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -2,9 +2,6 @@
 #array digits, where each digits[i] is the ith digit of the integer.
 #The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 #Increment the large integer by one and return the resulting array of digits.
-
-
-
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         return [int(digits)for digits in str(int(''.join(map(str,digits)))+1)]
