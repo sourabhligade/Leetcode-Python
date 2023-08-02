8bd980a8e4d2573b9139a6287174a3bcfb91e36e
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-08-02 15:56:42 +0530

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -3,6 +3,8 @@
 #The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 #Increment the large integer by one and return the resulting array of digits.
 
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         return [int(digits)for digits in str(int(''.join(map(str,digits)))+1)]
