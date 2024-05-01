41d898a12b9246c02870f5914681fac08f6915d2
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-01 22:50:46 +0530

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,6 +5,7 @@
 
 
 class Solution:
+    #quite easier on brute
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
