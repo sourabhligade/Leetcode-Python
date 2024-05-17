f319f4ba485c54f3169696ce74d05fc9f6c0a6a9
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-17 22:36:59 +0530

    Update 1108.Defanged IP address.py

--- 1108.Defanged IP address.py
+++ 1108.Defanged IP address.py
@@ -1,14 +1,1 @@
-Given a valid (IPv4) IP address, return a defanged version of that IP address.
-
-A defanged IP address replaces every period 
-"." with "[.]".
-
  
-
-
-
-
-class Solution:
-  def defangIPaddr(self,address):
-        return address.replace(".", "[.]")
-        
